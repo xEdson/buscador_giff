810b9d0faec5a39e368484f0add851ac8b444a78
xEdsonx <edson.nikicaro@gmail.com>
2019-09-12 15:40:30 -0300

    buscador de gifs

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:buscador_giff/ui/home_page.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+
+
+}
+
+
+
+
